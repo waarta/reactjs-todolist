e1eeb8451e12f8fe1a1cda28f81cf4871f803ee2
PERIN <perin011@pc02C.iut-rcc-info.urca>
2018-10-12 12:50:56 +0200

    fin tp

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import { createLogger } from "redux-logger";
+import "./App.css";
+import reducer from "./reducers";
+
+const middleware = [];
+
+if (ProcessingInstruction.env.NODE_ENV !== "production")
+	middleware.push(createLogger());
+
+const store = createStore(reducer, applyMiddleware(...middleware));
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<Provider store={store}>
+				<div className="App" />
+			</Provider>
+		);
+	}
 }
 
 export default App;
